9a8b200b998e0086ec98ceb37d5d5d41c8492d82
Teresa Pelinski <teresapelinski@gmail.com>
2020-03-15 13:25:48 +0100

    added comments

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,25 +4,28 @@ import './App.css';
 import CountryDetail from './components/CountryDetail';
 import countries from './countries.json';
 
-function App() {
-  const listaPaises = countries.map(e => e.name.common);
+
+// Una cosa chichos... A ver, Marc está explicando conforme a donde React se dirige
+
+// https://reactjs.org/docs/hooks-intro.html#gradual-adoption-strategy
+
+
+// y no lo hace mal, realmente os hace un gran favor explicando las cosas "a la última"
+// pero no es así como vais a encontrarlo en los ejercicios, que no se actualizan a la misma
+// velocidad de marc
+
+// He hecho unas modificaciones mierdsas, pero que espero que os ayuden a encaminar el ejercicio
+
+// a ver si así os ayuda a seguirlo, suerte!
+
+const App = (countries) => {
+  
+  const { ...countries } = useState();
+
+  //const listaPaises = countries.map(e => e.name.common);
   return (
     <div className="App">
       <CountryDetail />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
